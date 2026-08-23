a0f9a6a0f6e3be83e742329d37291d990c619367
hasib2019 <hasib.9437.hu@gmail.com>
2026-08-23 14:54:23 +0600

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,9 +3,10 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\Schema;
 class AppServiceProvider extends ServiceProvider
 {
+    
     /**
      * Register any application services.
      */
